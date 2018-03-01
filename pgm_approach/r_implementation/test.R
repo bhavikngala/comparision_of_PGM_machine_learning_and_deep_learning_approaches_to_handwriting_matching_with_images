edcd18ff825d91e53ea8f554ae1b644f6e032018
Bhavik N Gala <bhavikna@buffalo.edu>
2018-03-01 00:27:45 -0500

    batch extract descriptors and save them to file

--- pgm_approach/r_implementation/test.R
+++
@@ -1,7 +0,0 @@
-library(bnlearn)
-
-and_features = read.csv(file = './../../data/features.csv', header = TRUE, sep = ',')
-
-graph <- hc(and_features)
-
-plot(graph)
--- simple_ml_approach/sift_feature_extractor.py
+++ simple_ml_approach/sift_feature_extractor.py
@@ -2,26 +2,47 @@ import cv2
 import numpy as np
 import os
 
-def extractSIFTFeatures():
-	dataDir = './../../AND_dataset/Dataset[Without-Features]/AND_Images[WithoutFeatures]/'
-	
-	imgFilenames = os.listdir(dataDir)
-	sift = cv2.xfeatures2d.SIFT_create()
-	flen = 100000
-	
-	print('number of images:', len(imgFilenames))
+# directory of images
+dataDir = './../../AND_dataset/Dataset[Without-Features]/AND_Images[WithoutFeatures]/'
+# filename of SIFT descriptors
+siftDescriptorsFile = './../data/SIFTDescriptorDictionary.npy'
 
+# function extracts all the SIFT features of all the images in a directory
+# stores the features in a dictionary
+# stores the dictionary in a file for later use
+def extractSIFTFeatures():
+	# dictionary to store descriptors with filenames
+	descriptorDict = {}
+	
+	# list of all filenames in the directory
+	imgFilenames = os.listdir(dataDir)
+
+	# SIFT feature extractor
+	sift = cv2.xfeatures2d.SIFT_create()
+
+	# looping over all the images in the directory
+	# extracting SIFT features
+	# storing the feature in the dictionary
 	for imgFileName in imgFilenames:
+		# reading image as grayscale image
 		img = cv2.imread(dataDir + '/' + imgFileName, 0)
 	
-		kp, desc = sift.detectAndCompute(img, None)
-		if len(kp) < flen:
-			flen = len(kp)
+		# extracting keypoints and descriptors of image
+		_, desc = sift.detectAndCompute(img, None)
 
-		if len(kp) < 10:
-			print(imgFileName, ':::', len(kp))
+		writerID = imgFileName[0:4]
+		# initialize list or get existing list from dict
+		if writerID not in descriptorDict:
+			descList = [desc]
+		else:
+			descList = descriptorDict.get(writerID)
+			descList.append(desc)
 
-	print('min number of descriptors:', flen)
+		# update dictionary
+		descriptorDict.update({writerID:descList})
+
+	# store dictionary in file
+	np.save(siftDescriptorsFile, descriptorDict)
 
 def main():
 	extractSIFTFeatures()
